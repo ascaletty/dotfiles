b6d1d921a2bdb6be2b9e6337bdf5d101d86951bb
Aaron Scaletty <ascaletty23@gmail.com>
2025-07-27 15:08:35 -0500

    added qutebrowser config

--- Configs/nvim/.config/nvim/lua/plugins/example.lua
+++ Configs/nvim/.config/nvim/lua/plugins/example.lua
@@ -14,7 +14,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
  { "ellisonleao/gruvbox.nvim" },
@@ -80,6 +80,51 @@ return {
       },
     },
   },
+{
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+},
+  {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "UgandaBrain",
+        path = "~/projects/UgandaBrain/quartz/content/",
+      },
+      {
+        name = "work",
+        path = "~/projects/",
+      },
+    },
+
+    -- see below for full list of options 👇
+  },
+}
 
 
 
---
+++ Configs/qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,22 @@
+import os
+import tomllib
+config.load_autoconfig()
+
+c.colors.webpage.preferred_color_scheme= 'dark'
+def get_all_keys(d, prefix=[]):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            yield from get_all_keys(v, prefix + [k])
+        else:
+            yield (".".join(prefix + [k]), v)
+
+with open(f"{os.environ["HOME"]}/.config/qutebrowser/colors.toml", "rb") as f:
+    theme = tomllib.load(f)
+colors = theme["colors"]
+mappings = theme["mappings"]
+
+for key, value in get_all_keys(mappings, prefix=['colors']):
+    config.set(key, colors[value])
+config.bind(",w", "open https://web.archive.org/web/20250000000000*/{url}")
+config.bind("<Ctrl-c>", "mode-enter normal", mode='insert')
+# config.set('content.javascript.enabled', False)
